ac4cf4a7fa4fb3391ead9b0d7be9c7ca5a2bc569
André Masson <masson.andre@gmail.com>
2020-06-05 18:18:14 -0300

    Updates

---
+++ precache-manifest.38cb5c6bd1eac6a5c31cf40892fc2c30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96c981a7858994713f4a458bd2bf9efd",
+    "url": "/gravaudio/index.html"
+  },
+  {
+    "revision": "ce850e92100ffdda1a0d",
+    "url": "/gravaudio/static/css/main.c666ba74.chunk.css"
+  },
+  {
+    "revision": "392dbf165f1e8c3f43a2",
+    "url": "/gravaudio/static/js/2.9b699556.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gravaudio/static/js/2.9b699556.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce850e92100ffdda1a0d",
+    "url": "/gravaudio/static/js/main.88ce26b2.chunk.js"
+  },
+  {
+    "revision": "0839645e9a7227767ac4",
+    "url": "/gravaudio/static/js/runtime-main.d287372c.js"
+  }
+]);
